0e052a74d7fc7f9c1199536f085beaa0fd7c930a
金飞 <28206254@qq.com>
2017-04-18 16:00:14 +0800

    init

---
+++ examples/net/2.0/Tutorials/ConsoleApp/cs/src/LogEx/CommonLayout.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using log4net.Layout;
+using log4net.Layout.Pattern;
+using System.Reflection;
+using System.Collections;
+
+
+namespace ConsoleApp.LogEx
+{
+    public class CommonLayout : PatternLayout
+    {
+        public CommonLayout()
+        {
+            this.AddConverter("property", typeof(ReflectionPatternConverter));
+        }
+
+    }
+
+    public class ReflectionPatternConverter : PatternLayoutConverter
+    {
+        protected override void Convert(System.IO.TextWriter writer,log4net.Core.LoggingEvent loggingEvent)
+        {
+            if (Option != null)
+            {
+                // 写入指定键的值
+                WriteObject(writer,loggingEvent.Repository,LookupProperty(Option,loggingEvent));
+            }
+
+            else
+            {
+                // 写入所有关键值对
+                WriteDictionary(writer,loggingEvent.Repository,loggingEvent.GetProperties());
+            }
+
+        }
+
+        /// <summary>
+        /// 通过反射获取传入的日志对象的某个属性的值
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        private object LookupProperty(string property,log4net.Core.LoggingEvent loggingEvent)
+        {
+            object propertyValue = string.Empty;
+            PropertyInfo propertyInfo = loggingEvent.MessageObject.GetType().GetProperty(property);
+
+            if (propertyInfo != null)
+            {
+                propertyValue = propertyInfo.GetValue(loggingEvent.MessageObject, null);
+            }
+            return propertyValue;
+        }
+    }
+}
---
+++ examples/net/2.0/Tutorials/ConsoleApp/cs/src/LogEx/UpdateLogMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.LogEx
+{
+    class UpdateLogMessage
+    {
+        public string EntitySchemeName { get; set; }
+        public string EntityID { get; set; }
+        public string StringForOldEntity { get; set; }
+        public string StringForNewEntity { get; set; }
+        public UpdateLogMessage(string entitySchemeName, string entityID, string stringForOldEntity, string stringForNewEntity)
+        {
+            this.EntitySchemeName = entitySchemeName;
+            this.EntityID = entityID;
+            this.StringForOldEntity = stringForOldEntity;
+            this.StringForNewEntity = stringForNewEntity;
+        }
+    }
+}
